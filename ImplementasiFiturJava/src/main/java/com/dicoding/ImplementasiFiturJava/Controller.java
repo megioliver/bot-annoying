75c93d389b194fa1f182850e74f598f9448c35dd
Megi Oliver <megipramesti@gmail.com>
2020-12-23 11:08:37 +0700

    Add files via upload
    bad code, ok.

---
+++ ImplementasiFiturJava/src/main/java/com/dicoding/ImplementasiFiturJava/Controller.java
@@ -0,0 +1,125 @@
+package com.dicoding.ImplementasiFiturJava;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.client.LineSignatureValidator;
+import com.linecorp.bot.model.PushMessage;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.StickerMessage;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.objectmapper.ModelObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+
+
+@RestController
+public class Controller {
+
+
+    @Autowired
+    @Qualifier("lineMessagingClient")
+    private LineMessagingClient lineMessagingClient;
+
+
+    @Autowired
+    @Qualifier("lineSignatureValidator")
+    private LineSignatureValidator lineSignatureValidator;
+
+
+    @RequestMapping(value="/webhook", method= RequestMethod.POST)
+    public ResponseEntity<String> callback(
+            @RequestHeader("X-Line-Signature") String xLineSignature,
+            @RequestBody String eventsPayload)
+    {
+        try {
+            if (!lineSignatureValidator.validateSignature(eventsPayload.getBytes(), xLineSignature)) {
+                throw new RuntimeException("Invalid Signature Validation");
+            }
+
+
+            ObjectMapper objectMapper = ModelObjectMapper.createNewObjectMapper();
+            EventsModel eventsModel = objectMapper.readValue(eventsPayload, EventsModel.class);
+
+
+            eventsModel.getEvents().forEach((event)->{
+                if (event instanceof MessageEvent) {
+                    MessageEvent messageEvent = (MessageEvent) event;
+                    TextMessageContent textMessageContent = (TextMessageContent) messageEvent.getMessage();
+
+                    List<Message> msgArray = new ArrayList<>();
+                    msgArray.add(new TextMessage(textMessageContent.getText()));
+                    msgArray.add(new StickerMessage("1", "106"));
+                    ReplyMessage replyMessage = new ReplyMessage(((MessageEvent<?>) event).getReplyToken(), msgArray);
+                    reply(replyMessage);
+
+                    if(textMessageContent.getText().equalsIgnoreCase("sticker")){
+                        replySticker(messageEvent.getReplyToken(), "1", "106");
+                    }else{
+                        replyText(messageEvent.getReplyToken(), "Welcome");
+                    }
+                }
+
+            });
+
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @RequestMapping(value="/pushmessage/{id}/{message}", method=RequestMethod.GET)
+    public ResponseEntity<String> pushmessage(
+            @PathVariable("id") String userId,
+            @PathVariable("message") String textMsg
+    ){
+        TextMessage textMessage = new TextMessage(textMsg);
+        PushMessage pushMessage = new PushMessage(userId, textMessage);
+        push(pushMessage);
+
+
+        return new ResponseEntity<String>("Push message:"+textMsg+"\nsent to: "+userId, HttpStatus.OK);
+    }
+
+    private void reply(ReplyMessage replyMessage) {
+        try {
+            lineMessagingClient.replyMessage(replyMessage).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void replyText(String replyToken, String messageToUser){
+        TextMessage textMessage = new TextMessage(messageToUser);
+        ReplyMessage replyMessage = new ReplyMessage(replyToken, textMessage);
+        reply(replyMessage);
+    }
+
+    private void replySticker(String replyToken, String packageId, String stickerId){
+        StickerMessage stickerMessage = new StickerMessage(packageId, stickerId);
+        ReplyMessage replyMessage = new ReplyMessage(replyToken, stickerMessage);
+        reply(replyMessage);
+    }
+
+    private void push(PushMessage pushMessage){
+        try{
+            lineMessagingClient.pushMessage(pushMessage).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
